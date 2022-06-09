2cd86124a942f74e7f2bb212fbd113ec14beafc0
Roman Spector <ismailroma@ya.ru>
2022-06-09 18:41:31 +0300

    added db; fix show bind icon

--- ActionbarShift.lua
+++ ActionbarShift.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 ---@class ActionbarShift: Frame
 ---@field borderTopLeft ActionbarShiftBorderTopLeft
 ---@field borderTopRight ActionbarShiftBorderTopRight
@@ -9,7 +10,6 @@
 ---@field bg ActionbarShiftBg
 ---@field toggleButton ActionbarShiftToggleButton
 ---@field collapsed boolean
----@field buttons Button[]
 
 ---@class ActionbarShiftBorderTopLeft: Texture
 ---@class ActionbarShiftBorderTopRight: Texture
@@ -24,6 +24,7 @@
 
 local MultiBarRightButton1 = _G["MultiBarRightButton1"];
 local MultiBarLeftButton12 = _G["MultiBarLeftButton12"];
+local InterfaceOptionsActionBarsPanelRight = _G["InterfaceOptionsActionBarsPanelRight"];
 
 ---@param self ActionbarShift
 function ActionbarShift_Toggle(self)
@@ -46,7 +47,11 @@ function ActionbarShift_Expand(self)
     self.borderBottomRight:Show();
 
     self.toggleButton:GetNormalTexture():SetTexCoord(1, 0.5, 0, 1);
-    ActionbarShift_ShowButton(self)
+
+    MultiBarLeft:Show();
+    MultiBarRight:Show();
+
+    if ActionbarShiftDB then ActionbarShiftDB.visible = true;end
 end
 
 ---@param self ActionbarShift
@@ -61,28 +66,39 @@ function ActionbarShift_Collapse(self)
     self.borderBottomRight:Hide();
 
     self.toggleButton:GetNormalTexture():SetTexCoord(0.5, 0, 0, 1);
-    ActionbarShift_HideButton(self)
-end
 
----@param self ActionbarShift
-function ActionbarShift_ShowButton(self)
-    for iter, button in ipairs(self.buttons) do
-        button:Show();
-    end
-end
+    MultiBarLeft:Hide();
+    MultiBarRight:Hide();
 
----@param self ActionbarShift
-function ActionbarShift_HideButton(self)
-    for iter, button in ipairs(self.buttons) do
-        button:Hide();
-    end
+    if ActionbarShiftDB then ActionbarShiftDB.visible = false;end
 end
 
 ---@param self ActionbarShift
 function ActionbarShift_OnLoad(self)
-    self.buttons = {};
-    for i = 1, 12 do
-        table.insert(self.buttons, _G["MultiBarRightButton" .. i]);
-        table.insert(self.buttons, _G["MultiBarLeftButton" .. i]);
+    self:RegisterEvent("ADDON_LOADED");
+    ActionbarShift_Collapse(self);
+end
+
+---@param self ActionbarShift
+---@param event WowEvent
+---@param name string
+function ActionbarShift_OnEvent(self, event, name)
+    if name == "ActionbarShift" and ActionbarShiftDB.visible then
+        ActionbarShift_Expand(self);
     end
 end
+
+hooksecurefunc("MultiActionBar_Update", function()
+    local visible = ActionbarShiftDB and ActionbarShiftDB.visible;
+
+    if SHOW_MULTI_ACTIONBAR_3 and MainMenuBar.state=="player" and visible then
+        MultiBarRight:Show();
+    else
+        MultiBarRight:Hide();
+    end
+    if SHOW_MULTI_ACTIONBAR_3 and SHOW_MULTI_ACTIONBAR_4 and MainMenuBar.state=="player" and visible then
+        MultiBarLeft:Show();
+    else
+        MultiBarLeft:Hide();
+    end
+end)
