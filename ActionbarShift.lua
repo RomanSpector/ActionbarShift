3f5ecf056b49b8fe352f28253347ea1262bae49d
Roman Spector <ismailroma@ya.ru>
2022-06-10 17:27:20 +0300

    change textures

--- ActionbarShift.lua
+++ ActionbarShift.lua
@@ -1,18 +1,19 @@
 ---@diagnostic disable: undefined-global
 ---@class ActionbarShift: Frame
----@field borderTopLeft ActionbarShiftBorderTopLeft
----@field borderBottomLeft ActionbarShiftBorderBottomLeft
----@field borderLeft ActionbarShiftBorderLeft
----@field bg ActionbarShiftBg
+---@field tabTexture ActionbarShiftTabTexture
+---@field topHighlightTexture ActionbarShiftTopHighlightTexture
+---@field bottomHighlightTexture ActionbarShiftBottomHighlightTexture
+---@field leftHighlightTexture ActionbarShiftLeftHighlightTexture
 ---@field toggleButton ActionbarShiftToggleButton
 ---@field collapsed boolean
 
----@class ActionbarShiftBorderTopLeft: Texture
----@class ActionbarShiftBorderBottomLeft: Texture
----@class ActionbarShiftBorderLeft: Texture
----@class ActionbarShiftBg: Texture
+---@class ActionbarShiftTabTexture: Texture
+---@class ActionbarShiftTopHighlightTexture: Texture
+---@class ActionbarShiftBottomHighlightTexture: Texture
+---@class ActionbarShiftLeftHighlightTexture: Texture
 
----@class ActionbarShiftToggleButton: Button
+---@class ActionbarShiftToggleButton: Frame
+---@field texture Texture
 
 local function updateContainersAnchors()
     local frame, xOffset, yOffset, screenHeight, freeScreenHeight, leftMostPoint, column;
@@ -94,7 +95,7 @@ function ActionbarShift_Expand(self)
     self.collapsed = false;
 
     self:SetPoint("RIGHT", MultiBarLeftButton6, "LEFT", 0, 0);
-    self.toggleButton:GetNormalTexture():SetTexCoord(1, 0.5, 0, 1);
+    self.toggleButton.texture:SetTexCoord(1, 0.5, 0, 1);
 
     MultiBarLeft:Show();
     MultiBarRight:Show();
@@ -109,8 +110,8 @@ end
 function ActionbarShift_Collapse(self)
     self.collapsed = true;
 
-    self:SetPoint("RIGHT", MultiBarLeftButton6, "LEFT", 90, 0);
-    self.toggleButton:GetNormalTexture():SetTexCoord(0.5, 0, 0, 1);
+    self:SetPoint("RIGHT", MultiBarLeftButton6, "LEFT", 87, 0);
+    self.toggleButton.texture:SetTexCoord(0.5, 0, 0, 1);
 
     MultiBarLeft:Hide();
     MultiBarRight:Hide();
@@ -144,10 +145,71 @@ local function hookMultiActionBar_Update()
     end
 end
 
+local function SetClampedTextureRotation(texture, rotationDegrees)
+    if (rotationDegrees ~= 0 and rotationDegrees ~= 90 and rotationDegrees ~= 180 and rotationDegrees ~= 270) then
+        error("SetRotation: rotationDegrees must be 0, 90, 180, or 270");
+        return;
+    end
+
+    if not (texture.rotationDegrees) then
+        texture.origTexCoords = {texture:GetTexCoord()};
+        texture.origWidth = texture:GetWidth();
+        texture.origHeight = texture:GetHeight();
+    end
+
+    if (texture.rotationDegrees == rotationDegrees) then
+        return;
+    end
+
+    texture.rotationDegrees = rotationDegrees;
+
+    if (rotationDegrees == 0 or rotationDegrees == 180) then
+        texture:SetWidth(texture.origWidth);
+        texture:SetHeight(texture.origHeight);
+    else
+        texture:SetWidth(texture.origHeight);
+        texture:SetHeight(texture.origWidth);
+    end
+
+    if (rotationDegrees == 0) then
+        texture:SetTexCoord( texture.origTexCoords[1], texture.origTexCoords[2],
+                                            texture.origTexCoords[3], texture.origTexCoords[4],
+                                            texture.origTexCoords[5], texture.origTexCoords[6],
+                                            texture.origTexCoords[7], texture.origTexCoords[8] );
+    elseif (rotationDegrees == 90) then
+        texture:SetTexCoord( texture.origTexCoords[3], texture.origTexCoords[4],
+                                            texture.origTexCoords[7], texture.origTexCoords[8],
+                                            texture.origTexCoords[1], texture.origTexCoords[2],
+                                            texture.origTexCoords[5], texture.origTexCoords[6] );
+    elseif (rotationDegrees == 180) then
+        texture:SetTexCoord( texture.origTexCoords[7], texture.origTexCoords[8],
+                                            texture.origTexCoords[5], texture.origTexCoords[6],
+                                            texture.origTexCoords[3], texture.origTexCoords[4],
+                                            texture.origTexCoords[1], texture.origTexCoords[2] );
+    elseif (rotationDegrees == 270) then
+        texture:SetTexCoord( texture.origTexCoords[5], texture.origTexCoords[6],
+                                            texture.origTexCoords[1], texture.origTexCoords[2],
+                                            texture.origTexCoords[7], texture.origTexCoords[8],
+                                            texture.origTexCoords[3], texture.origTexCoords[4] );
+    end
+end
+
 ---@param self ActionbarShift
 function ActionbarShift_OnLoad(self)
     hooksecurefunc("MultiActionBar_Update", hookMultiActionBar_Update);
     hooksecurefunc("updateContainerFrameAnchors", hookUpdateContainerFrameAnchors);
+
+    local rotationDegrees = 270;
+    local colorTable = { r = 1, g = 0.5, b = 0.25 };
+
+    SetClampedTextureRotation(self.tabTexture, rotationDegrees);
+    SetClampedTextureRotation(self.topHighlightTexture, rotationDegrees);
+    SetClampedTextureRotation(self.leftHighlightTexture, rotationDegrees);
+    SetClampedTextureRotation(self.bottomHighlightTexture, rotationDegrees);
+
+    self.topHighlightTexture:SetVertexColor(colorTable.r, colorTable.g, colorTable.b);
+    self.leftHighlightTexture:SetVertexColor(colorTable.r, colorTable.g, colorTable.b);
+    self.bottomHighlightTexture:SetVertexColor(colorTable.r, colorTable.g, colorTable.b);
 
     self:RegisterEvent("ADDON_LOADED");
 end
